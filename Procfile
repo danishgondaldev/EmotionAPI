12bbb73abb28087b658d933cc2897cb296f05420
danishgondaldev <danishakramgondal@gmail.com>
2024-12-14 16:55:27 +0500

    Updated Deployment Files

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn wsgi:app
+web: gunicorn emotion_api:app
--- wsgi.py
+++
@@ -1,4 +0,0 @@
-from emotion_api import app  # This assumes the Flask app is inside `emotion_api.py`
-
-if __name__ == "__main__":
-    app.run()
